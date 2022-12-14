cd3e1e367fe91776a267531a6919976cc75c5a1f
carlos Oliveira <carlosmdohk@gmail.com>
2022-12-13 22:34:22 -0300

    primeiro commit

---
+++ hello-typescript/main.ts
@@ -0,0 +1,14 @@
+var minhaVariavel = 'Teste minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+var num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor * 2;
+});
+numeros.map( valor =>);
