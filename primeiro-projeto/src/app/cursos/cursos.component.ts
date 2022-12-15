daa186bdb39f7ab0b171529d98553923c3bd6567
carlos Oliveira <carlosmdohk@gmail.com>
2022-12-14 21:56:43 -0300

    criando serviços

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
+
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit{
+
+  nomePortal: string;
+
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService) { 
+    this.nomePortal = 'http://loiane.training';
+
+    /*for ( let i=0; i<this.cursos.length; i++){
+      let cursos = this.cursos[i];
+    }*/
+
+    //var servico = new CursosService();
+    this.cursos = this.cursosService.getCursos();
+  }
+  ngOnInit(){
+    
+  }
 
 }
